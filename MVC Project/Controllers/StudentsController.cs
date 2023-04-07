9df4c9ec8d5e30c4025b72c6935e65d3e3903182
Bilish12 <bil.kharbuja100@gmail.com>
2023-04-07 06:51:27 +0545

    assignment6

---
+++ MVC Project/Controllers/StudentsController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Project.Models;
+
+namespace MVC_Project.Controllers
+{
+    public class Data
+    {
+        public static List<Student> model = new List<Student>()
+        {
+            new Student { Id = 01, Name = "Suman Wick", Faculty = "BIT" },
+            new Student { Id = 02, Name = "Deepak Gaire", Faculty = "BCA" }
+
+        };
+    }
+    public class StudentsController : Controller
+    {
+        // GET: StudentsController
+        public ActionResult Index()
+        {
+            var model = Data.model;
+            return View(model);
+        }
+
+        // GET: StudentsController/Details/5
+        public ActionResult Details(int Id)
+        {
+            Student model = Data.model.Single(stu => stu.Id == Id);
+            return View(model);
+        }
+        // GET: StudentsController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student student)
+        {
+            Data.model.Add(student);
+            return RedirectToAction("");
+        }
+
+        // GET: StudentsController/Edit/5
+        public ActionResult Edit(int Id)
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Student stu)
+        {
+            Student student = Data.model.Single(s => s.Id == stu.Id);
+            student.Name = stu.Name;
+            student.Faculty = stu.Faculty;
+            return RedirectToAction("");
+        }
+
+        // GET: StudentsController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: StudentsController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Student stu)
+        {
+            Student delStudent = Data.model.Single(s => s.Id ==stu.Id);
+            Data.model.Remove(delStudent);
+             return RedirectToAction("");
+            
+        }
+    }
+}
